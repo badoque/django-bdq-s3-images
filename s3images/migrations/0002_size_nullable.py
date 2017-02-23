60b6cbfbd40917430424eb0444a9c003c81bc54d
hugobessa <hugo@bessa.me>
2017-02-22 21:10:09 -0300

    fix: typo

--- s3images/migrations/0002_size_nullable.py
+++ s3images/migrations/0002_size_nullable.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AlterField(
             model_name='ExternalImage',
-            name='size'
+            name='size',
             field=models.IntegerField(blank=True, null=True),
         ),
     ]
